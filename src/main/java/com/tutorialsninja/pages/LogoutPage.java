67730e18f77f54d5d79fe15d45c0af8ede8d6929
pateldb08 <pateldb08@gmail.com>
2023-05-02 23:29:59 +0100

    Week-13-project-2-com-tutorialsninja-sw4

---
+++ src/main/java/com/tutorialsninja/pages/LogoutPage.java
@@ -0,0 +1,11 @@
+package com.tutorialsninja.pages;
+import com.tutorialsninja.utilities.Utility;
+import org.openqa.selenium.By;
+
+public class LogoutPage extends Utility {
+    By logoutText = By.xpath("//h1[contains(text(),'Account Logout')]");
+
+    public String getTextFromLogout() {
+        return getTextFromElement(logoutText);
+    }
+}
